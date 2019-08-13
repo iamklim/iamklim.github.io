f3fdde9d0ebc5aefdf03a3b2a195437b51d55183
iamklim <drkleem@gmail.com>
2019-08-13 14:42:09 +0300

    Experimenting with fetch - then

--- src/App.js
+++ src/App.js
@@ -20,92 +20,148 @@ class App extends Component {
         const OMDbAPI = "55018c43";
         const that = this;
 
-        let TMDbNowPlaying = `https://api.themoviedb.org/3/movie/now_playing?region=UA&language=ru-RU&api_key=${TMDbAPI}`;
-        let posterPath = `https://image.tmdb.org/t/p/original`; // lower resolution: https://image.tmdb.org/t/p/w370_and_h556_bestv2
+        const receiveMovieItems = async () => {
+            const TMDbNowPlaying = `https://api.themoviedb.org/3/movie/now_playing?region=UA&language=ru-RU&api_key=${TMDbAPI}`;
+            const posterPath = `https://image.tmdb.org/t/p/original`; // lower resolution: https://image.tmdb.org/t/p/w370_and_h556_bestv2
 
-        fetch(TMDbNowPlaying, {
-            method: 'get',
-        }).then(function(response) {
-            return response.json();
-        }).then(function(jsonData) {
+            const response = await fetch(TMDbNowPlaying, {method: 'get'});
+            const jsonData = await response.json();
             const responseResults = jsonData.results;
+
             responseResults.forEach((item) => {
                 item.poster = posterPath + item.poster_path;
             });
-            that.setState({movies: responseResults});
 
-            let newState = [...that.state.movies];
+            return responseResults;
+        }
 
-            for (let i = 0; i < that.state.movies.length; i++) {
-                let currTMDbId = that.state.movies[i].id;
-                let currTMDbMovieInfo = `https://api.themoviedb.org/3/movie/${currTMDbId}?api_key=${TMDbAPI}&append_to_response=external_ids,videos&language=ru-RU`;
+        const updateMovieInfo = async (index) => {
+            let currMovie = that.state.movies[index];
+            let currTMDbId = currMovie.id;
+            let currTMDbMovieInfo = `https://api.themoviedb.org/3/movie/${currTMDbId}?api_key=${TMDbAPI}&append_to_response=external_ids,videos&language=ru-RU`;
 
-                fetch(currTMDbMovieInfo, {
-                    method: 'get',
-                }).then(function (response) {
-                    return response.json();
-                }).then(function (jsonData) {
-                    let currIMDbId = jsonData.imdb_id;
+            const response = await fetch(currTMDbMovieInfo, {method: 'get'});
+            const jsonData = await response.json();
 
-                    let trailers = jsonData.videos.results,
-                        lastTrailerUrl = '';
-                    if (trailers.length > 0) {
-                        let lastTrailer = trailers[trailers.length - 1];
-                        if (lastTrailer.site === "YouTube") {
-                            lastTrailerUrl = `https://www.youtube.com/watch?v=${lastTrailer.key}`;
-                        }
-                    }
-
-                    let currGenres = jsonData.genres;
-
-                    newState[i].imdb_id = currIMDbId;
-                    newState[i].trailer_url = lastTrailerUrl;
-                    newState[i].genres = currGenres;
-
-                    let currOMDbMovieInfo = `http://www.omdbapi.com/?i=${currIMDbId}&apikey=${OMDbAPI}`;
-                    return fetch(currOMDbMovieInfo, {
-                        method: 'get',
-                    })
-                }).then(function (response) {
-                    return response.json();
-                }).then(function(jsonData) {
-                    if (isNaN(jsonData.imdbRating)) {
-                        jsonData.imdbRating = '-';
-                    }
-
-                    newState[i].imdbRating = jsonData.imdbRating; // may be "-"
-                    newState[i].director = jsonData.Director;
-                    newState[i].actors = jsonData.Actors;
-                    newState[i].metascore = jsonData.Metascore; // may be "N/A"
-                })
-
-                .catch(function(error) {
-                    console.log('Request failed', error)
-                });
+            let currIMDbId = jsonData.imdb_id;
+            let trailers = jsonData.videos.results,
+                lastTrailerUrl = '';
+            if (trailers.length > 0) {
+                let lastTrailer = trailers[trailers.length - 1];
+                if (lastTrailer.site === "YouTube") {
+                    lastTrailerUrl = `https://www.youtube.com/watch?v=${lastTrailer.key}`;
+                }
             }
+            let currGenres = jsonData.genres;
 
-            newState.sort(function (a, b) {
-                if (a.imdbRating > b.imdbRating) {
-                    return -1;
-                }
-                else if (a.imdbRating < b.imdbRating) {
-                    return 1;
-                }
-                else {
-                    return 0;
-                }
-            });
+            currMovie.imdb_id = currIMDbId;
+            currMovie.trailer_url = lastTrailerUrl;
+            currMovie.genres = currGenres;
 
-            that.setState({ movies: newState, contentLoaded: true });
+            return currMovie;
+        }
 
-        }).catch(function(error) {
-            console.log('Request failed', error)
-        })
+        const updateMovies = () => {
+            let newState = [];
+            for (let i = 0; i < that.state.length; i++) {
+                //await updateMovieInfo(i);
+                console.log(1111111111111);
+            }
+            // return newState;
+            console.log(that.state.movies)
+        }
+
+        const updateState = (newData) => {
+            that.setState({movies: newData})
+        }
+
+        receiveMovieItems()
+            .then(responseResults => updateState(responseResults))
+            .then(() => updateMovies() )
+
+
+        // const updateMovieInfo = (index) => {
+        //
+        //     let currMovie = that.state.movies[index];
+        //     let currTMDbId = currMovie.id;
+        //     let currTMDbMovieInfo = `https://api.themoviedb.org/3/movie/${currTMDbId}?api_key=${TMDbAPI}&append_to_response=external_ids,videos&language=ru-RU`;
+        //
+        //     fetch(currTMDbMovieInfo, {
+        //         method: 'get',
+        //     }).then(function (response) {
+        //         return response.json();
+        //     }).then(function (jsonData) {
+        //         let currIMDbId = jsonData.imdb_id;
+        //
+        //         let trailers = jsonData.videos.results,
+        //             lastTrailerUrl = '';
+        //         if (trailers.length > 0) {
+        //             let lastTrailer = trailers[trailers.length - 1];
+        //             if (lastTrailer.site === "YouTube") {
+        //                 lastTrailerUrl = `https://www.youtube.com/watch?v=${lastTrailer.key}`;
+        //             }
+        //         }
+        //
+        //         let currGenres = jsonData.genres;
+        //
+        //         currMovie.imdb_id = currIMDbId;
+        //         currMovie.trailer_url = lastTrailerUrl;
+        //         currMovie.genres = currGenres;
+        //
+        //         let currOMDbMovieInfo = `http://www.omdbapi.com/?i=${currIMDbId}&apikey=${OMDbAPI}`;
+        //         return fetch(currOMDbMovieInfo, {
+        //             method: 'get',
+        //         })
+        //     }).then(function (response) {
+        //         return response.json();
+        //     }).then(function(jsonData) {
+        //         if (isNaN(jsonData.imdbRating)) {
+        //             jsonData.imdbRating = '-';
+        //         }
+        //
+        //         currMovie.imdbRating = jsonData.imdbRating; // may be "-"
+        //         currMovie.director = jsonData.Director;
+        //         currMovie.actors = jsonData.Actors;
+        //         currMovie.metascore = jsonData.Metascore; // may be "N/A"
+        //
+        //         return currMovie;
+        //     }).catch(function(error) {
+        //         console.log('Request failed', error)
+        //     });
+        //
+        // }
+        //
+        // const updateMovies = () => {
+        //     for (let i = 0; i < that.state.movies.length; i++) {
+        //         updateMovieInfo(i)
+        //     }
+        // }
+        //
+        // const sortArray = () => {
+        //     let newState = [...that.state.movies];
+        //
+        //     newState.sort(function (a, b) {
+        //         if (a.imdbRating > b.imdbRating) {
+        //             return -1;
+        //         }
+        //         else if (a.imdbRating < b.imdbRating) {
+        //             return 1;
+        //         }
+        //         else {
+        //             return 0;
+        //         }
+        //     });
+        //
+        //     return newState;
+        // }
+        //
+        // const updateState = (newState) => {
+        //     that.setState({ movies: newState, contentLoaded: true });
+        // }
 
     }
 
     render() {
-        console.log(this.state.movies)
         return (
             <>
                 {/*<Preloader contentLoaded={this.state.contentLoaded}/>*/}
