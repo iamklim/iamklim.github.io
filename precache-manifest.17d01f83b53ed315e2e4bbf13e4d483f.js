9a0bc82a1f969ebdaa8734b820d9485e6b4f4331
etoklim <vlad.klymenko@softermii.com>
2020-04-22 12:19:54 +0300

    Updates

---
+++ precache-manifest.17d01f83b53ed315e2e4bbf13e4d483f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511f61e0388c1161e1be0e27c679eead",
+    "url": "/now-playing/index.html"
+  },
+  {
+    "revision": "74dc02946fc494f9bcf3",
+    "url": "/now-playing/static/css/2.09700f6a.chunk.css"
+  },
+  {
+    "revision": "bf72c523e2976525554a",
+    "url": "/now-playing/static/css/main.5eebd471.chunk.css"
+  },
+  {
+    "revision": "74dc02946fc494f9bcf3",
+    "url": "/now-playing/static/js/2.3240adfe.chunk.js"
+  },
+  {
+    "revision": "bf72c523e2976525554a",
+    "url": "/now-playing/static/js/main.cfb9c2bf.chunk.js"
+  },
+  {
+    "revision": "60e9fe858ac57da52faf",
+    "url": "/now-playing/static/js/runtime~main.5a95b424.js"
+  },
+  {
+    "revision": "75723dc5222bcd11ef7c53e4a8aebb5c",
+    "url": "/now-playing/static/media/10751.75723dc5.png"
+  },
+  {
+    "revision": "767a2178ef98bf0f85e6931595b56c40",
+    "url": "/now-playing/static/media/10770.767a2178.png"
+  },
+  {
+    "revision": "2af82424c8f343486b5f52034baff0f4",
+    "url": "/now-playing/static/media/12.2af82424.png"
+  },
+  {
+    "revision": "bb3991f783b5826cbdc2b364ff8f52be",
+    "url": "/now-playing/static/media/27.bb3991f7.png"
+  },
+  {
+    "revision": "8b2ce634d3d296be0af39b3faa6d3f47",
+    "url": "/now-playing/static/media/99.8b2ce634.png"
+  },
+  {
+    "revision": "604207281204a777f027da4c782f6b46",
+    "url": "/now-playing/static/media/emojii-facepalm.60420728.png"
+  },
+  {
+    "revision": "b9aba793000ceb549410b487769e642e",
+    "url": "/now-playing/static/media/empty-poster.b9aba793.png"
+  },
+  {
+    "revision": "73d796179189b2f87f896a1153d3fa3e",
+    "url": "/now-playing/static/media/youtube.73d79617.svg"
+  }
+]);
